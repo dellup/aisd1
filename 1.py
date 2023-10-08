60d68d87ffda6f99c6daea400d30a1f443c8e106
dellup <139969481+dellup@users.noreply.github.com>
2023-10-08 17:16:44 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,40 @@
 from random import *
 from timeit import timeit
-def hairbrush(li):
-    n = len(li)
-    step = n
-    while step > 1 or flag:
-        if step > 1:
-            step = int(step / 1.247331)
-        flag = False
-        i = 0
-        while i + step < n:
-            if li[i] > li[i + step]:
-                li[i], li[i + step] = li[i + step], li[i]
-                flag = True
-            i+=1
-    return li
 a = [randint(1, 10000) for i in range(50)]
-print("Массив до сортировки: " + a)
-print("Массив после сортировки: " + hairbrush(a))
-time = timeit(hairbrush, number=1)
-print("Время выполнения = "+time)
+b=a.copy()
+def hairbrush(a):
+	n = len(a)
+	step = n
+	while step > 1 or flag:
+		if step > 1:
+			step = int(step / 1.247331)
+		flag = False
+		i = 0
+		while i + step < n:
+			if a[i] > a[i + step]:
+				a[i], a[i + step] = a[i + step], a[i]
+				flag = True
+			i+=1
+	return a
+
+test="""
+from random import randint
+a = [randint(1, 10000) for i in range(50)]
+n = len(a)
+step = n
+while step > 1 or flag:
+    if step > 1:
+        step = int(step / 1.247331)
+    flag = False
+    i = 0
+    while i + step < n:
+        if a[i] > a[i + step]:
+            a[i], a[i + step] = a[i + step], a[i]
+            flag = True
+        i+=1
+"""
+
+print("Массив до сортировки: ",  b)
+print("Массив после сортировки: ", hairbrush(b))
+timre = timeit(test, number=1)
+print("Время выполнения = ", timre)
